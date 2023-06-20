0f050161bf1d591c5eb987ca20bc9eb5b5feb89e
brady-pittman <96319266+brady-pittman@users.noreply.github.com>
2023-06-20 13:27:13 -0400

    Update desirability_rate.py

--- desirability_rate.py
+++ desirability_rate.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-data = pd.read_csv('C:\\Users\\602353\\Downloads\\FanGraphs Leaderboard (2).csv')
+data = pd.read_csv('C:\\Users\\602353\\Downloads\\FanGraphs Leaderboard (2015+).csv')
 
 data_no_nan = data.dropna()
 
@@ -15,10 +15,16 @@ data_no_team['Soft%'] = pd.to_numeric(data_no_team['Soft%'].str.replace('%',''))
 
 data_no_team['Desirability Rate'] = data_no_team['K%'] + data_no_team['GB%'] + data_no_team['Soft%'] - data_no_team['Hard%'] - data_no_team['BB%'] - data_no_team['Barrel%']
 
-data_sorted = data_no_team.sort_values(by='Desirability Rate', ascending=True)
+data_sorted = data_no_team.sort_values(by='Desirability Rate', ascending=False)
 
-average_rate = data_no_team['Desirability Rate'].mean()
+average_rate = data_no_team['Desirability Rate'].mean().round(1)
+
+print(data_no_team.loc[data_no_team['Name'] == 'Chris Sale', ['Name', 'Desirability Rate']])
+print()
 
 print('Average Desirability Rate: {}'.format(average_rate))
+print()
 
-print(data_sorted.head(15))
+pd.set_option('display.max_columns', 2)
+
+print(data_sorted.head())
