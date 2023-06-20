2c4434d23fc6c2c3e84e3ddd06e493c79ae7161c
brady-pittman <96319266+brady-pittman@users.noreply.github.com>
2023-06-20 10:48:52 -0400

    Add files via upload
    Program to calculate the career (K+GB) - (BB+Barrel's) rate of MLB pitchers

---
+++ fangraphs.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+
+data = pd.read_csv('C:\\Users\\602353\\Downloads\\FanGraphs Leaderboard (1).csv')
+
+data_no_nan = data.dropna()
+
+data_no_team = data_no_nan.drop(['Team', 'playerid', 'mlbamid'], axis=1)
+
+data_no_team['K%'] = pd.to_numeric(data_no_team['K%'].str.replace('%',''))
+data_no_team['BB%'] = pd.to_numeric(data_no_team['BB%'].str.replace('%',''))
+data_no_team['GB%'] = pd.to_numeric(data_no_team['GB%'].str.replace('%',''))
+data_no_team['Barrel%'] = pd.to_numeric(data_no_team['Barrel%'].str.replace('%',''))
+
+data_no_team['(K+GB) - (BB+BRL)'] = data_no_team['K%'] + data_no_team['GB%'] - data_no_team['BB%'] - data_no_team['Barrel%']
+
+data_sorted = data_no_team.sort_values(by='(K+GB) - (BB+BRL)', ascending=False)
+
+average_rate = data_no_team['(K+GB) - (BB+BRL)'].mean()
+
+print(average_rate)
+
+print(data_sorted.head())
